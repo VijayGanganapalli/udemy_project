3abea353205edf506122193825ff06be0f632f29
Vijay <vijayganganapalli@gmail.com>
2018-09-08 11:27:22 +0530

    login screen added

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -6,6 +7,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Udemy Proect",
       theme: ThemeData(primarySwatch: Colors.green),
+      home: LoginScreen(),
     );
   }
 }
