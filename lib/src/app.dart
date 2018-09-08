b21b111c713d368bbcd6d750a24d1f659692676c
Vijay <vijayganganapalli@gmail.com>
2018-09-08 11:17:17 +0530

    app file added

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Udemy Proect",
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
