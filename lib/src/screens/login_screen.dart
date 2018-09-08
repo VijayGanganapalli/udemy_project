297089c92b3f8e99a2f3a5b9f4cc3e35f5a4c67c
Vijay <vijayganganapalli@gmail.com>
2018-09-08 11:40:28 +0530

    email, password textfields added

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -4,7 +4,29 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        margin: EdgeInsets.all(18.0),
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(labelText: "Email"),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(labelText: "Password"),
     );
   }
 }
